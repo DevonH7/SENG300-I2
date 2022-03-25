1850bed16bad52afc67eabf315542699ef7265d7
Kevin Van <vankevin58@gmail.com>
2022-03-25 17:34:41 -0600

    Observer Implementation

--- SCS - Software/src/controller/MembershipScan.java
+++ SCS - Software/src/controller/MembershipScan.java
@@ -1,7 +1,53 @@
 package controller;
 
 import org.lsmr.selfcheckout.*;
+import org.lsmr.selfcheckout.Card.CardData;
+import org.lsmr.selfcheckout.devices.AbstractDevice;
+import org.lsmr.selfcheckout.devices.CardReader;
+import org.lsmr.selfcheckout.devices.observers.AbstractDeviceObserver;
+import org.lsmr.selfcheckout.devices.observers.CardReaderObserver;
+public class MembershipScan implements CardReaderObserver{
 
-public class MembershipScan {
+	@Override
+	public void enabled(AbstractDevice<? extends AbstractDeviceObserver> device) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void disabled(AbstractDevice<? extends AbstractDeviceObserver> device) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void cardInserted(CardReader reader) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void cardRemoved(CardReader reader) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void cardTapped(CardReader reader) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void cardSwiped(CardReader reader) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void cardDataRead(CardReader reader, CardData data) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
